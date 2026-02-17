4e12c9dd9b0df1e852dac861a11bb2c2aec6112d
EC2 Default User <ec2-user@ip-172-31-30-197.eu-north-1.compute.internal>
2026-02-17 20:48:03 +0000

    Added production VPC configuration

---
+++ infra/terraform/vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "production-vpc"
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
+}
