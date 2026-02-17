e6668f52bc4f454438e8a69968caee0302ec371f
EC2 Default User <ec2-user@ip-172-31-30-197.eu-north-1.compute.internal>
2026-02-17 21:59:26 +0000

    Added EKS node group

---
+++ infra/terraform/eks-node-group.tf
@@ -0,0 +1,30 @@
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "production-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = [
+    aws_subnet.private_subnet.id
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  instance_types = ["t3.micro"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_policy_1,
+    aws_iam_role_policy_attachment.node_policy_2,
+    aws_iam_role_policy_attachment.node_policy_3
+  ]
+
+  tags = {
+    Name        = "production-node-group"
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
+}
+
--- infra/terraform/subnet.tf
+++ infra/terraform/subnet.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-north-1a"
+  availability_zone = "eu-north-1b"
 
   tags = {
     Name        = "production-private-subnet"
