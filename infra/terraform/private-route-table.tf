e1470e4e15e2dc503fbbb09737d58ea6aec0e18b
EC2 Default User <ec2-user@ip-172-31-30-197.eu-north-1.compute.internal>
2026-02-17 21:22:25 +0000

    Added private route table and NAT routing

---
+++ infra/terraform/private-route-table.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name        = "production-private-route-table"
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
