a10d5c9bc62724e94e55f8b6fce3c3faffdd5e9d
EC2 Default User <ec2-user@ip-172-31-30-197.eu-north-1.compute.internal>
2026-02-17 21:09:42 +0000

    Added public route table and association

---
+++ infra/terraform/route-table.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name        = "production-public-route-table"
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
