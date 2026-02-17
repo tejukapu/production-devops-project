a298c6756e30b7db72253a655f44fcf001077971
EC2 Default User <ec2-user@ip-172-31-30-197.eu-north-1.compute.internal>
2026-02-17 22:45:25 +0000

    Added Kubernetes deployment and service manifests

--- infra/terraform/eks-node-group.tf
+++ infra/terraform/eks-node-group.tf
@@ -13,7 +13,7 @@ resource "aws_eks_node_group" "eks_node_group" {
     min_size     = 1
   }
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   depends_on = [
     aws_iam_role_policy_attachment.node_policy_1,
